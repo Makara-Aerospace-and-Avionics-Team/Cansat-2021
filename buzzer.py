f7f0e9d9340c6d933c4068c0d9c6a8d30c13baf6
haekal1002 <muhammadhaekal333@gmail.com>
2021-11-11 17:44:03 +0800

    kodingan buzzer

---
+++ buzzer.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+buzzer_pin = 23
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(buzzer_pin, GPIO.OUT, initial=GPIO.LOW)
+
+# ketika output altitude dari bmp 388 bernilai 0
+# maka buzzer akan menyala
+if (some_altitude_function == 0):
+    GPIO.output(buzzer_pin, GPIO.HIGH)
+    sleep(5) # buzzes for 5 secs
+    GPIO.output(buzzer_pin, GPIO.LOW)
+    
+GPIO.cleanup()
