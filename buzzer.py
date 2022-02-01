87f8e67a7dc9d2ed8d5b62f71d7e822fd20d9cc1
Muhammad Malik Ardiansyah <71066812+malik188@users.noreply.github.com>
2022-02-01 10:48:40 +0700

    Update buzzer.py

--- buzzer.py
+++ buzzer.py
@@ -9,7 +9,7 @@ GPIO.setup(buzzer_pin, GPIO.OUT, initial=GPIO.LOW)
 
 # ketika output altitude dari bmp 388 bernilai 0
 # maka buzzer akan menyala
-if (some_altitude_function == 0):
+while (some_altitude_function == 0):
     GPIO.output(buzzer_pin, GPIO.HIGH)
     sleep(5) # buzzes for 5 secs
     GPIO.output(buzzer_pin, GPIO.LOW)
