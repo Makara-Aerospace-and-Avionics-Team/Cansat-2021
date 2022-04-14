6d413fe768224256a957809964690a574f52b420
haekal-alg <58630571+haekal-alg@users.noreply.github.com>
2022-04-14 17:23:45 +0700

    Kode buzzer

---
+++ CODE YANG BARU (TEENSY)/buzzer.cpp
@@ -0,0 +1,20 @@
+int buzzerPin = 13;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() {
+  if (some_altitude_function() == 0) {
+    // buzzes for 5 seconds
+    digitalWrite(buzzerPIN, HIGH);
+    delay(5000);
+    digitalWrite(buzzerPIN, LOW);
+  }
+}
+
+// placeholder function
+int some_altitude_function() {
+  return 0;
+}
